dfa5fc9999ac3914c4491e428ceb1cb069e944df
PigPigLetsGo <105574230+PigPigLetsGo@users.noreply.github.com>
2026-09-19 18:13:32 +0800

    Update tools array in initWidget configuration
    Removed 'switch-model' and 'switch-texture' from tools array.

--- autoload.js
+++ autoload.js
@@ -36,8 +36,10 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
+			//原:https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/
 			cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
-			tools: ["asteroids", "switch-model", "switch-texture", "photo", "quit"]
+			// "switch-model", "switch-texture", 
+			tools: ["asteroids","photo", "quit"]
 		});
 	});
 }
